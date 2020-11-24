3087ae55cdb51f10257416fe62b55ef28fe9fff2
Tom <sp9rt77w@gmail.com>
2020-11-24 16:44:39 +0800

    linerLayout&onClick

--- app/src/main/java/com/tom/practice_1124/MainActivity.kt
+++ app/src/main/java/com/tom/practice_1124/MainActivity.kt
@@ -2,10 +2,26 @@ package com.tom.practice_1124
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ed_weight
+        ed_hight
+        b_help
+
+    }
+    fun bmi(view: View) {
+        val weight =ed_weight.text.toString().toFloat()
+        val height =ed_hight.text.toString().toFloat()
+        val bmi = weight/(height*height)
+        Log.d("BMI", bmi.toString())
     }
 }
